e1a24b5ae6e5bc59fce04362b534e4c721674e28
berolog <berolog360@gmail.com>
2022-02-21 14:54:22 +0200

    Added vars directory. Added shared library for 2 pipeline

---
+++ vars/pipeline2.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy
+
+def call() {
+    node {
+        def cfg = pipelineCfg()
+
+        try {
+            stage('Checkout') {
+                git cfg.gitRepo
+            }
+
+            stage('Build') {
+                sh cfg.buildCommand
+            }
+
+            stage('Test') {
+                sh "${cfg.testCommand} > ${cfg.failureArtifact}"
+            }
+
+            currentBuild.result = 'SUCCESS'
+        }
+
+        catch(e) {
+            archiveArtifacts artifacts: cfg.failureArtifact
+            currentBuild.result = 'FAILURE'
+            throw e
+        }
+
+        finally {
+            writeFile file: cfg.alwaysArtifact, text: currentBuild.result
+            archiveArtifacts artifacts: cfg.alwaysArtifact
+        }
+}
