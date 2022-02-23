6ee21d4bad1638e36b1a3f83212b984f23093c55
berolog <berolog360@gmail.com>
2022-02-23 16:09:31 +0200

    changed testStep for all pipelines #1

--- vars/testStep.groovy
+++ vars/testStep.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
-    sh './test.sh > test_results.txt'
+def call(String mvn=null) {
+    if (mvn == null) {
+        sh './test.sh > test_results.txt'
+    }
+    else {
+        sh "${mvn} test"
+    }
 }
