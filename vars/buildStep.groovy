9aaea352063023c0bba1406e9038943d72a916b4
berolog <berolog360@gmail.com>
2022-02-23 16:02:22 +0200

    changed buildStep for all pipelines #1

--- vars/buildStep.groovy
+++ vars/buildStep.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
-def call() {
-    sh './build.sh'
+def call(String mvn=null) {
+    if (mvn == null) {
+        sh './build.sh'
+    } 
+    
+    else {
+        sh 'mvn clean install'
+        sh 'mvn hpi:hpi'
+    }
 }
